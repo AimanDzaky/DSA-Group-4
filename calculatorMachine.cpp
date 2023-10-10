951063f0212830ec153c4724fe98ba0721b368af
AvellinoVincent <147046450+AvellinoVincent@users.noreply.github.com>
2023-10-10 20:39:04 +0700

    calculatorMachine.cpp

--- calculatorMachine.cpp
+++ calculatorMachine.cpp
@@ -121,3 +121,120 @@ double calculatorMachine(string input){
 		}
 		input[i] = tolower(input[i]);
 	}
+
+//INITIALIZE mathObject ARRAY
+	mathObject arr[input.length()];
+
+//COVERT string INPUT TO mathObject ARRAY
+
+	// > BRACKET TELEPORT SYSTEM
+	int bracketDepth = 0;
+	int bracketTeleportHandler[20];
+
+	// > INITIALIZE LOOP VARIABLES
+
+	int i = 0, //INDEX being worked on
+		t = 0; //target INDEX of mathObject arr
+
+// > MAIN LOOP PARSING THE INPUT
+    while(i < input.length()){
+    	char cur = input[i];
+    	if(cur == '('){
+    		arr[t] = mathObject("bracket_open", 98);
+    		bracketDepth++;
+    		bracketTeleportHandler[bracketDepth] = t;
+    		t++; i++;
+
+		}else if(cur == ')'){
+    		arr[t] = mathObject("bracket_close", -1);
+    		arr[bracketTeleportHandler[bracketDepth]].setNum(t);
+    		bracketDepth--;
+    		t++; i++;
+
+		}else if(cur == '^'){
+			arr[t] = mathObject("power", 6);
+    		t++; i++;
+
+		}else if(cur == '*'){
+			arr[t] = mathObject("multiplication", 4);
+    		t++; i++;
+
+		}else if(cur == '/'){
+			arr[t] = mathObject("division", 3);
+    		t++; i++;
+
+		}else if(cur == '+'){
+			arr[t] = mathObject("addition", 2);
+    		t++; i++;
+
+		}else if(cur == '-'){
+			arr[t] = mathObject("substraction", 1);
+    		t++; i++;
+
+		}else if('0' <= cur && cur <= '9'){
+			double frontTemp = 0; //infront of coma
+			double backTemp = 0; // back of coma
+			double behindComa = 0;
+			while(true){
+
+				if(input[i] == '.'){
+					behindComa = 1;
+				}else if(behindComa > 0){
+					int toInt = input[i] - '0';
+					backTemp = (backTemp*10) + toInt;
+					behindComa++;
+				}else{
+					int toInt = input[i] - '0';
+					frontTemp = (frontTemp*10) + toInt;
+				}
+
+
+				char nextInput = input[i+1];
+				if((i+1) < input.length() && (('0' <= nextInput && nextInput <= '9') || nextInput == '.')){
+					i++;
+				}else{
+					arr[t] = mathObject("number", 99, frontTemp+(backTemp/pow(10,behindComa-1)) );
+					t++; i++;
+					break;
+				}
+			}
+
+		}else if('a' <= cur && cur <= 'z'){
+			int startIn = i;
+			while(true){
+				char nextInput = input[i+1];
+				if((i+1) < input.length() && ('a' <= nextInput && nextInput <= 'z')){
+					i++;
+				}else{
+					string str = input.substr(startIn, i-startIn+1);
+					if(str == "sin") arr[t] = mathObject("sin", 5);
+					else if(str == "cos") arr[t] = mathObject("cos", 5);
+					else if(str == "tan") arr[t] = mathObject("tan", 5);
+					else if(str == "log") arr[t] = mathObject("log", 5);
+					else if(str == "sqrt") arr[t] = mathObject("sqrt", 5);
+					else if(str == "ln") arr[t] = mathObject("ln", 5);
+					else if(str == "torad") arr[t] = mathObject("torad", 5);
+					else{
+						cout << "<!!!> ERROR: UNKNOWN FUNCTION" << endl;
+						throw;
+					}
+					t++; i++;
+					break;
+				}
+			}
+		}else{
+			cout << "<!!!> ERROR: UNRECOGNIZED INPUT" << endl;
+			break;
+		}
+	}
+
+	if(bracketDepth != 0){
+		cout << "<!!!> ERROR: UNEVEN BRACKET" << endl;
+		throw;
+	}
+
+	
+	//INVOKE THE SOLVE RECURSION ON THE PARSED INPUT!!!!!
+	double toReturn = solveRecursion(arr, 0, t-1);
+	return toReturn;
+}
